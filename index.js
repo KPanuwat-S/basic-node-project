f45346a6027e26eb73a93f29af4518f06c878fb0
KPanuwat-S <panuwatbood@gmail.com>
2023-04-11 05:38:24 +0700

    Start Node Project

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+///////////////////////////////////////////////////////////////
+// FILES
+// // Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}`;
+
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log(`File written`);
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2}\n${data3}`,
+//         "utf-8",
+//         (err) => {console.log('Your file has been written');}
+//       );
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+///////////////////////////////////////////////////////////////
+// Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObject = JSON.parse(data);
+console.log(`Data`, data);
+console.log(`DataObj`, dataObject);
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log(`Listening to request on port 8000`);
+});
